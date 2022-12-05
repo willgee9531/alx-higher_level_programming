f570f1807c76581b0803c4276da080152e6e6737
willgee9531 <willgee9531@gmail.com>
2022-12-05 14:01:15 -0800

    task 6 done

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        count = 0
+        for elem in row:
+            count += 1
+            print('{:d}'.format(elem), end=(" " if count < len(row) else ""))
+        print()
