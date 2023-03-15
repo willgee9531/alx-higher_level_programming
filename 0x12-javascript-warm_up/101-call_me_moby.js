96aec1acbaf54286404bec6a432e1fd8756a5d99
willgee9531 <willgee9531@gmail.com>
2023-03-14 17:21:02 -0700

    hot fix 10 and 101

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) theFunction();
+  for (let i = 0; i < x; i++)
+  {
+      console.log(theFunction());
+  }
 };
-
