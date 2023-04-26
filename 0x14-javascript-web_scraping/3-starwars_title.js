b126cedd68fbcb072dd03d62402ed0a3338ff2f7
willgee9531 <willgee9531@gmail.com>
2023-04-26 05:27:53 -0700

    starwars fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
+
+/* prints the title of a Star Wars movie where episode number matches a given integer */
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
+
+if (process.argv.length >= 3) {
+  const _id = process.argv[2];
+  const url = 'https://swapi-api.alx-tools.com/api/films/' + _id;
+  request(url, (err, response, body) => {
+    if (err) throw err;
+    const data = JSON.parse(body);
+    console.log(data.title);
+  });
+}
